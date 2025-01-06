9932680739e36b6643f5c3e5e392ce1e091fc588
HectorHS <kelanved@gmail.com>
2025-01-07 00:08:24 +0200

    updating router

--- src/router/index.ts
+++ src/router/index.ts
@@ -10,7 +10,7 @@ const Experiments = () => import('@/views/Experiments.vue')
 const Current = () => import('@/views/Current.vue')
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(),
   routes: [
     {
       path: '/',
